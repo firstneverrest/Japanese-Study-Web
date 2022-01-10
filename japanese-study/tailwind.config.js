140630aaeb6be131ebf2f8d96aefc5bae46206c7
Chitsanupong <first30952@gmail.com>
2022-01-10 12:16:17 +0700

    feat: set up tailwindcss

---
+++ japanese-study/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#DD919F',
+        secondary: '#D46C86',
+        tertiary: '#3682C6',
+        error: '#F44336',
+        warning: '#FF9800',
+        info: '#2196F3',
+        success: '#4CAF50',
+      },
+    },
+  },
+  plugins: [],
+};
